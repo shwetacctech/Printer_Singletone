19879c9021c0ef8baceb88a5dead36cd940d8e17
Shweta <shweta.raut@cctech.co.in>
2024-02-06 18:35:32 +0530

    Add project files.

---
+++ Printer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer_Singletone
+{
+    public class Printer
+    {
+        public static Printer Printer_Instance;
+
+        //creating instance of printer object
+        public static Printer GetInstance()
+        {
+            if( Printer_Instance== null)
+            {
+                Printer_Instance = new Printer(); 
+            }
+            return Printer_Instance;
+
+        }
+        //function to print data
+        public void PrintData(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer_Singletone
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            //Creating instance of printer object for printer1
+            Printer printer1= Printer.GetInstance();
+            printer1.PrintData("I'm printed in first instance");
+
+            //Creating instance of printer object for printer2
+            Printer printer2 = Printer.GetInstance();
+            printer1.PrintData("I'm printed in Second instance");
+
+            Console.WriteLine(printer1 ==  printer2);
+            Console.ReadKey();
+
+        }
+    }
+}
